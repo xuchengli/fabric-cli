ce4ff82adcda69d5fc856a5cb89c19d147105f7c
xuchengli <lixucheng@aliyun.com>
2019-12-16 14:58:29 +0800

    add docker version list and install func

--- index.js
+++ index.js
@@ -3,30 +3,31 @@ const Server = require('./lib/server');
 
 log.useLabels = true;
 
-const server = new Server('39.104.189.169', 'root', 'Baas-mgt-passw0rd');
-server.checkDocker().then(result => {
+async function installDocker() {
+  const server = new Server('106.75.66.107', 'root', '!Baas@test');
+  await server.removeDocker(out => {
+    console.log(out);
+  }, err => {
+    console.error(err);
+  });
+  const availableDockerVersions = await server.listAvailableDockerVersions('amd64',
+    out => {
+      console.log(out);
+    },
+    err => {
+      console.error(err);
+    }
+  );
+  log.debug(availableDockerVersions);
+  await server.installDocker('5:18.09.9~3-0~ubuntu-xenial', out => {
+    console.log(out);
+  }, err => {
+    console.error(err);
+  });
+  return await server.checkDocker();
+}
+installDocker().then(result => {
   log.success(result);
-}).catch(err => {
-  log.error(err);
-});
-
-const server2 = new Server('39.104.51.94', 'root', 'Baas-mgt-passw0rd');
-server2.checkDocker().then(result => {
-  log.success(result);
-}).catch(err => {
-  log.error(err);
-});
-
-const server3 = new Server('106.75.36.113', 'root', '!Baas@test');
-server3.checkDocker().then(result => {
-  log.success(result);
-}).catch(err => {
-  log.error(err);
-});
-
-const server4 = new Server('106.75.70.223', 'root', '!Baas@test');
-server4.removeDocker().then(result => {
-  log.debug(result);
 }).catch(err => {
   log.error(err);
 });
