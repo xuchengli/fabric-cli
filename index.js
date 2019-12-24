c90980973a005c325353e493e12513f510774885
xuchengli <lixucheng@aliyun.com>
2019-12-24 16:19:45 +0800

    add docker update function

--- index.js
+++ index.js
@@ -6,12 +6,6 @@ const { installDocker } = require('./lib/install-docker');
 
 log.useLabels = true;
 
-// await server.removeDocker(out => {
-//   loader.writeBuffer(out);
-// }, err => {
-//   loader.writeBuffer(err);
-// });
-
 console.log(chalk.blueBright(figlet.textSync('Play with fabric blockchain', {
   font: 'ANSI Shadow',
   horizontalLayout: 'fitted'
@@ -19,7 +13,7 @@ console.log(chalk.blueBright(figlet.textSync('Play with fabric blockchain', {
 
 const server = new Server('106.75.66.107', 'root', '!Baas@test');
 
-installDocker(server).then(result => {
+installDocker(server, '18.09.5').then(result => {
   log.success(result);
 }).catch(err => {
   log.error(err);
