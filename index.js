235523e4634fe9a3f3da23beef0abb0edafded48
xuchengli <lixucheng@aliyun.com>
2019-12-26 17:44:56 +0800

    add docker swarm for network layer

--- index.js
+++ index.js
@@ -1,8 +1,8 @@
 const log = require('log-beautify');
 const chalk = require('chalk');
 const figlet = require('figlet');
-const Server = require('./lib/server');
-const { installDocker } = require('./lib/install-docker');
+// const Server = require('./lib/server');
+// const { installDocker } = require('./lib/install-docker');
 
 log.useLabels = true;
 
@@ -11,10 +11,19 @@ console.log(chalk.blueBright(figlet.textSync('Play with fabric blockchain', {
   horizontalLayout: 'fitted'
 })));
 
-const server = new Server('106.75.66.107', 'root', '!Baas@test');
+// const server = new Server('106.75.66.107', 'root', '!Baas@test');
 
-installDocker(server, '18.09.5').then(result => {
-  log.success(result);
-}).catch(err => {
-  log.error(err);
-});
+// installDocker(server, '18.09.5').then(result => {
+//   log.success(result);
+// }).catch(err => {
+//   log.error(err);
+// });
+
+// server.checkDockerSwarm().then(result => {
+//   const { LocalNodeState, Error } = JSON.parse(result);
+//   if (LocalNodeState === 'inactive' && !Error) {
+//     log.success('Docker Swarm可用>>>>');
+//   }
+// }).catch(err => {
+//   log.error(err);
+// });
